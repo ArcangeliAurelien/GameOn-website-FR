6a848e2e851ecfb6723363eeb5f0a883c1c80643
Aurélien Arcangeli <arkaurel5929@gmail.com>
2022-01-28 00:19:49 +0100

    modif html + fermeture de la modale + début des validations des inputs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,6 +14,8 @@ const formData = document.querySelectorAll(".formData");
 
 const closeModalBtn = document.querySelector("#closeModal");
 
+const submit = document.querySelector("#submit");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -28,3 +30,42 @@ function closeModal() {
 }
 
 closeModalBtn.addEventListener("click", closeModal);
+
+submit.addEventListener("click", validate);
+
+// Implémenter les entrées du formulaire
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+const location1 = document.getElementById('location1');
+const location2 = document.getElementById('location2');
+const location3 = document.getElementById('location3');
+const location4 = document.getElementById('location4');
+const location5 = document.getElementById('location5');
+const location6 = document.getElementById('location6');
+const checkbox1 = document.getElementById('checkbox1');
+
+
+function validate() {
+
+  //Masquer les messages
+  let tooltip = document.querySelectorAll('.tooltip');			//Sélectionne l'ensemble des éléments à cacher
+
+  for (var i = 0; i < tooltip.length; i++) {						//La méthode utilisée pour cacher les messages d'erreur
+    tooltip[i].style.display = 'none';
+  }
+
+
+  //Vérifier le prénom
+  if (first.value.length < 2 || first.value == "") {
+    tooltip[0].style.display = 'inline';
+    prenom.className = "incorrect";
+  }else {
+    tooltip[0].style.display = 'none';
+    prenom.className = "correct";
+  }
+
+
+}
