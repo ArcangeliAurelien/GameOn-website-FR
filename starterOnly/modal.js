2eba4c5d68c3956de857434c7e31756d67cad260
Aurélien Arcangeli <arkaurel5929@gmail.com>
2022-01-27 12:50:50 +0100

    fermer la modale

---
+++ starterOnly/modal.js
@@ -0,0 +1,30 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+const closeModalBtn = document.querySelector("#closeModal");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// fermeture de la modale
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+closeModalBtn.addEventListener("click", closeModal);
